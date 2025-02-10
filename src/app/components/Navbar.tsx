a8350d4bdcf7edca24fbcf3d9fa9b2c52bc9a83e
SayaniSen2022 <109034346+SayaniSen2022@users.noreply.github.com>
2025-02-10 23:28:40 +0530

    nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="bg-white text-black text-center pt-10">
@@ -36,14 +40,49 @@ const Navbar = () => {
         </div>
 
         <div>
-          <ul className="flex justify-center gap-4 uppercase text-black pt-9 pb-2">
-            <li>Home</li>
-            <li>Shop</li>
-            <li>Collections</li>
-            <li>Contact Us</li>
-            <li>Final Sale</li>
-            <li>Gift Cards</li>
-            <li>Shop Our Live Sales</li>
+          <ul className="flex justify-center gap-4 uppercase text-black text-sm pt-9 pb-2">
+            <li className="hover:underline underline-offset-4 decoration-solid cursor-pointer">
+              Home
+            </li>
+            <li
+              className="relative hover:underline underline-offset-4 decoration-solid cursor-pointer"
+              onMouseEnter={() => setIsOpen(true)}
+              onMouseLeave={() => setIsOpen(false)}
+            >
+              <div className="flex items-center">
+                Shop
+                <span className="pl-1">
+                  <IoIosArrowDown />
+                </span>
+              </div>
+
+              {isOpen && (
+                <div className="absolute left-0 mt-0 w-48 bg-white shadow-lg rounded-md text-sm">
+                  <ul className="py-2 pl-2 text-left">
+                    <li className="px-2 py-2 hover:bg-gray-100">Beauty</li>
+                    <li className="px-2 py-2 hover:bg-gray-100">Apparel</li>
+                    <li className="px-2 py-2 hover:bg-gray-100">Accessories</li>
+                    <li className="px-2 py-2 hover:bg-gray-100">Jewellery</li>
+                    <li className="px-2 py-2 hover:bg-gray-100">Sale</li>
+                  </ul>
+                </div>
+              )}
+            </li>
+            <li className="hover:underline underline-offset-4 decoration-solid cursor-pointer">
+              Collections
+            </li>
+            <li className="hover:underline underline-offset-4 decoration-solid cursor-pointer">
+              Contact Us
+            </li>
+            <li className="hover:underline underline-offset-4 decoration-solid cursor-pointer">
+              Final Sale
+            </li>
+            <li className="hover:underline underline-offset-4 decoration-solid cursor-pointer">
+              Gift Cards
+            </li>
+            <li className="hover:underline underline-offset-4 decoration-solid cursor-pointer">
+              Shop Our Live Sales
+            </li>
           </ul>
         </div>
       </div>
